cd81fa5b57e1ab3845c3253c6de94eab1757597b
Kawinda Jayasundara <134467909+HasithMbiz@users.noreply.github.com>
2026-09-11 17:14:56 +0530

    fixed iOS permission issues

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -4,6 +4,6 @@ import 'utils/app_configs.dart';
 import 'utils/enums/flavors.dart';
 
 Future<void> main() async {
-  AppConfigs.initializeFlavor(flavor: Flavor.dev, showFlavorBanner: true, baseUrl: 'http://124.43.79.70:5000');
+  AppConfigs.initializeFlavor(flavor: Flavor.dev, showFlavorBanner: true, baseUrl: 'http://169.58.42.245:5000');
   mainConfigs();
 }
